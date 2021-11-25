59fc4645fa11b3ac4572e7ad7ee9100a1518454c
ArditS <42702899+arditsulceteaching@users.noreply.github.com>
2021-11-25 15:18:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our first Web App.
+Enjoy it!
+''')
